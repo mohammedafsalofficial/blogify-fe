a40d802f98fd3c7208ae85cd6023c668cc88f590
Mohammed Afsal <mohammedafsal.a@codingmart.com>
2025-05-15 13:32:25 +0530

    Initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'blogify-fe' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blogify-fe');
+    expect(app.applicationName).toEqual('Blogify');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blogify-fe');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, blogify-fe'
+    );
   });
 });
